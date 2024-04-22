5315d2e77f404d6f1c0be07d2a8bd43941665aa2
aandino186 <aandino186@gmail.com>
2024-04-22 17:51:59 +0000

    Fix naming for layout

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
            Ridin App
         </Title>
     </Head>
-    <Header />
     <section class="flex max-w-5xl gap-4 w-fit flex-wrap mx-auto mt-5">
         <Card 
            v-for="(car, index) in cars"
@@ -23,7 +22,10 @@
 <script setup >
     import Card from '../components/Card.vue';
     import cars from '../mockApi/cars.json'
-    import  Header  from '../components/Header.vue'
+
+    definePageMeta({
+        layout: 'default'
+    })
 
 
 </script>
